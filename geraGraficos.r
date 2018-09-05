9e1b32b2b39bf548bc56978f1749be6db483485c
Giovani Bossoni <giovanibossoni@hotmail.com>
2018-09-05 12:24:12 -0300

    convertendo data

--- geraGraficos.r
+++ geraGraficos.r
@@ -1,9 +1,10 @@
 library("ggplot2")
+library("zoo")
 
-dados <- read.csv("tabelaTuplas.csv")
-
-
+dados$teste <- as.yearmon(paste(dados$year, dados$month), "%Y %m")
 dados$ordem <- dados$month + (dados$year*2)
+
+
 
 field0 <- subset(dados,field == 0,select = c(field,month,year,production,age,ordem,tempo))
 
@@ -16,3 +17,7 @@ agregadoProdField <-aggregate(dados$production,by = list(dados$field),mean)
 ggplot(agregadoProdAge,aes(x=agregadoProdAge$Group.1,y = agregadoProdAge$x)) + geom_line() + labs(x= "Idade" , y = "Produção")
 
 ggplot(field0,aes(x=field0$ordem,y=field0$production)) + geom_smooth()
+
+ggplot(agregadoProdField,aes(x=agregadoProdField$Group.1,y = agregadoProdField$x,colour =agregadoProdField$x),) + geom_bar(stat = "identity",aes(fill = agregadoProdField$x)) + labs(x= "Field" , y = "Produção") + labs(colour = "Produção")
+
+
